aef8507d4daf1a92ed725bb0e1b7ed808f0474e6
darrenmce <darren.mcelligott@gmail.com>
2017-11-20 12:33:38 -0500

    use async/await and add pods formatter

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const Api = require('kubernetes-client');
 const { createServer } = require('./lib/server.js');
+const { createK8s } = require('./lib/k8s');
 
-const core = new Api.Core(Api.config.getInCluster());
-const server = createServer({ k8sCore: core });
+const apiConfig = Api.config.getInCluster();
+const core = new Api.Core({
+  ...apiConfig,
+  promises: true,
+});
+const k8s = createK8s(core);
+
+const server = createServer({ k8s });
 
 server.listen(process.env.PORT || 8080, () => {
   console.log('mustard up and running...');
