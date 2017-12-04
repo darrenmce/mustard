92f08941d7b7e2ccb02d47657ad4d58a5ba05e35
darrenmce <darren.mcelligott@gmail.com>
2017-12-04 11:41:49 -0500

    add configmap support and inflight deduping for deploy and delete

--- client/lib/main.jsx
+++ client/lib/main.jsx
@@ -7,9 +7,13 @@ function Main({ pods, deployments, baseUrl }) {
     <div>
       <div>
         <h2>New Deployment</h2>
-        <form action={`${baseUrl}/deploy`}>
+        <form action={`${baseUrl}/deploy`} method="POST">
           <input name="appName" type="text" placeholder="application name..." />
           <input name="image" type="text" placeholder="image..." />
+          <div>
+            <p>{'JSON Config (will be mounted at /etc/{application name}/config)'}</p>
+            <textarea name="config" cols="30" rows="10" />
+          </div>
           <input type="submit" value="Deploy" />
         </form>
       </div>
